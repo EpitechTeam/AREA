c33be3b400a2da5601de280f734ddc6a95625280
lecherbonnier <leo.lecherbonnier@epitech.eu>
2019-02-18 20:47:17 +0100

    test

--- server/services/facebook/facebook.service.js
+++ server/services/facebook/facebook.service.js
@@ -131,30 +131,8 @@ let verifyWebhook = async (req, res) => {
 let webhook = async (req, res) => {
 	let body = req.body;
 
-	// Checks this is an event from a page subscription
-	// console.log(body.entry[0].changed_fields);
-	// if (body.entry[0].changed_fields[0] == 'events') {
-	// 	console.log(body.entry[0]);
-	// }
-	console.log(body);
-	console.log(body.entry);
-	if (body.object === 'page') {
-
-		// Iterates over each entry - there may be multiple if batched
-		body.entry.forEach(function(entry) {
-
-			// Gets the message. entry.messaging is an array, but
-			// will only ever contain one message, so we get index 0
-			let webhook_event = entry.messaging[0];
-			console.log(webhook_event);
-		});
-
-		// Returns a '200 OK' response to all requests
-		res.status(200).send('EVENT_RECEIVED');
-	} else {
-		// Returns a '404 Not Found' if event is not from a page subscription
-		res.sendStatus(404);
-	}
+	console.log(body.changes);
+	res.status(200).send('EVENT_RECEIVED');
 }
 
 module.exports = {
