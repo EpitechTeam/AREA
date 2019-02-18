4328778efb954772e23de289da7aa6ab382454aa
lecherbonnier <leo.lecherbonnier@epitech.eu>
2019-02-18 21:33:40 +0100

    FIX: event if

--- server/services/facebook/facebook.service.js
+++ server/services/facebook/facebook.service.js
@@ -135,7 +135,7 @@ let webhook = async (req, res) => {
 	if (body.entry[0].changes[0].field == 'events') {
 		let newFacebook = new FacebookSpec.Facebook("null");
 		await newFacebook.setAccessTokenByUserId(body.entry[0].id);
-		await newFacebook.getInfoEvent();
+		await newFacebook.getInfoEvent(body.entry[0].changes[0].value.event_id);
 	}
 	res.status(200).send('EVENT_RECEIVED');
 }
