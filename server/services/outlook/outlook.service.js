312d906b641e710564fe68614337180e4fab1aae
lecherbonnier <leo.lecherbonnier@epitech.eu>
2019-02-19 15:28:58 +0100

    FIX : getMe

--- server/services/outlook/outlook.service.js
+++ server/services/outlook/outlook.service.js
@@ -18,21 +18,21 @@ let getEmail = async (req, res) => {
 let getMyOption = async (req, res) => {
 	let newOutlook = new OutlookSpec.Outlook(req.token);
 
-	let option = await OutlookSpec.Outllook.getMyOption();
+	let option = await newOutlook.getMyOption();
 	res.json({data : option})
 }
 
 let addFileToOne_drive = async (req, res) => {
 	let newOutlook = new OutlookSpec.Outlook(req.token);
 
-	await OutlookSpec.Outlook.setFileToOneDrive();
+	await newOutlook.setFileToOneDrive();
 	res.json({type : true})
 }
 
 let getMe = async (req, res) => {
 	let newOutlook = new OutlookSpec.Outlook(req.token);
 
-	let me = await OutlookSpec.Outlook.getMe();
+	let me = await newOutlook.getMe();
 	res.json({type : true, data : me})
 }
 
