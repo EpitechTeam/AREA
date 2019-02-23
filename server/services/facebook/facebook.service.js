3fbcbcaaf9662b6c90a7504ce5173f9ca6dd8bb3
lecherbonnier <leo.lecherbonnier@epitech.eu>
2019-02-23 11:03:24 +0100

    TEST : likes facebook

--- server/services/facebook/facebook.service.js
+++ server/services/facebook/facebook.service.js
@@ -131,6 +131,7 @@ let verifyWebhook = async (req, res) => {
 let webhook = async (req, res) => {
 	let body = req.body;
 
+	console.log(body.entry[0].changes[0].field)
 	if (body.entry[0].changes[0].field == 'events') {
 		let newFacebook = new FacebookSpec.Facebook("null");
 		await newFacebook.setAccessTokenByUserId(body.entry[0].id);
