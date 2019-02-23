304fafc6335c47ddafc8f115da985bde63c4441c
lecherbonnier <leo.lecherbonnier@epitech.eu>
2019-02-23 15:37:55 +0100

    TEST : see photos object

--- server/services/facebook/facebook.service.js
+++ server/services/facebook/facebook.service.js
@@ -139,6 +139,7 @@ let webhook = async (req, res) => {
 
 	if (body.entry[0].changes[0].field == 'photos') {
 		console.log(body.entry[0]);
+		console.log(body.entry[0].changes[0]);
 		let newFacebook = new FacebookSpec.Facebook("null");
 		await newFacebook.setAccessTokenByUserId(body.entry[0].id);
 		//await newFacebook.handlePhotos(body.entry[0].changes[0].value.event_id, body.entry[0].id);
