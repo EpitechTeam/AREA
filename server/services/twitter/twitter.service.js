5b8862ca66e2b5e5c6beab8d907cecb721dc07fd
lecherbonnier <leo.lecherbonnier@epitech.eu>
2019-02-23 19:40:33 +0100

    ADD : TwitterSpec

--- server/services/twitter/twitter.service.js
+++ server/services/twitter/twitter.service.js
@@ -2,6 +2,7 @@ let ObjectId	= require('mongodb').ObjectID
 let User	= require('./../../models/User')
 let config  = require('../../config/index')
 let serviceConfig = require('../../config/service')
+let TwitterSpec = require('./twitter.spec');
 
 let addTwitterConnection = async (req, res) => {
 	let newTwitter = new TwitterSpec.Twitter(req.token);
