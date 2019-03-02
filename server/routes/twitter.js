d8a9db30c2314de53b24a24e6e7b7934b462ab20
lecherbonnier <leo.lecherbonnier@epitech.eu>
2019-03-02 14:19:38 +0100

    FIX : get to put

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -28,24 +28,24 @@ router.get('/webhook', twitterService.crc)
 
 router.get('/create-webhook', ensureAuthorized, twitterService.createWebhook)
 
-router.get('/addTweetByMail', ensureAuthorized, twitterService.addTweetByMail)
+router.put('/addTweetByMail', ensureAuthorized, twitterService.addTweetByMail)
 
-router.get('/removeTweetByMail', ensureAuthorized, twitterService.addTweetByMail)
+router.put('/removeTweetByMail', ensureAuthorized, twitterService.addTweetByMail)
 
-router.get('/addStartFollowByMail', ensureAuthorized, twitterService.addStartFollowByMail)
+router.put('/addStartFollowByMail', ensureAuthorized, twitterService.addStartFollowByMail)
 
-router.get('/removeStartFollowByMail', ensureAuthorized, twitterService.removeStartFollowByMail)
+router.put('/removeStartFollowByMail', ensureAuthorized, twitterService.removeStartFollowByMail)
 
-router.get('/addGetFollowByMail', ensureAuthorized, twitterService.addGetFollowByMail)
+router.put('/addGetFollowByMail', ensureAuthorized, twitterService.addGetFollowByMail)
 
-router.get('/removeGetFollowByMail', ensureAuthorized, twitterService.removeGetFollowByMail)
+router.put('/removeGetFollowByMail', ensureAuthorized, twitterService.removeGetFollowByMail)
 
-router.get('/addGetUnfollowByMail', ensureAuthorized, twitterService.addGetUnfollowByMail)
+router.put('/addGetUnfollowByMail', ensureAuthorized, twitterService.addGetUnfollowByMail)
 
-router.get('/removeGetUnfollowByMail', ensureAuthorized, twitterService.removeGetUnfollowByMail)
+router.put('/removeGetUnfollowByMail', ensureAuthorized, twitterService.removeGetUnfollowByMail)
 
-router.get('/addStartFollowSendDirectMessage', ensureAuthorized, twitterService.addStartFollowSendDirectMessage)
+router.put('/addStartFollowSendDirectMessage', ensureAuthorized, twitterService.addStartFollowSendDirectMessage)
 
-router.get('/removeStartFollowSendDirectMessage', ensureAuthorized, twitterService.removeStartFollowSendDirectMessage)
+router.put('/removeStartFollowSendDirectMessage', ensureAuthorized, twitterService.removeStartFollowSendDirectMessage)
 
 module.exports = router
