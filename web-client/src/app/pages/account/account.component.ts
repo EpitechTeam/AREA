1b0da1be1760a614eaf32dac4c03b4c69abb9b2d
David Zakrzewski <david.zakrzewski@epitech.eu>
2019-03-04 17:59:41 +0100

    Done: Account page

--- web-client/src/app/pages/account/account.component.ts
+++ web-client/src/app/pages/account/account.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../user.service';
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+    selector: 'app-account',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+    firstName = '';
+    lastName = '';
+    email = '';
+    changePassword = false;
+    password = '';
+    confirmPassword = '';
 
-  ngOnInit() {
-  }
+    badPasswordClass = '';
+
+    constructor(private userService: UserService) {
+        this.firstName = this.userService.getUser().first_name;
+        this.lastName = this.userService.getUser().last_name;
+        this.email = this.userService.getUser().email;
+        console.log(this.userService.getUser());
+    }
+
+    ngOnInit() {
+    }
+
+    editPassword() {
+        this.changePassword = true;
+    }
+
+    editPasswordProcedure() {
+        if (this.password === '' || this.confirmPassword === '' || this.password !== this.confirmPassword) {
+            this.badPasswordClass = 'badPassword';
+            return;
+        }
+        if (this.password === this.confirmPassword) {
+            this.userService.changeUserPassword(this.password).then();
+            this.changePassword = false;
+        }
+    }
+
+    saveChanges() {
+        this.userService.updateUser(this.firstName, this.lastName, this.email).then();
+    }
+
+    onChanged(event) {
+        if (this.badPasswordClass !== '') {
+            this.badPasswordClass = '';
+        }
+    }
 
 }
